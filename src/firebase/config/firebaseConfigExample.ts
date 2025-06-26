030382d260f1e3b79ef75e165889daddc8695ff7
nicolasjveiga <nickjveiga@gmail.com>
2025-06-26 11:22:16 -0300

    Stop tracking firebaseConfig.ts

---
+++ src/firebase/config/firebaseConfigExample.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "seu-projeto.firebaseapp.com",
+  projectId: "seu-projeto",
+  storageBucket: "seu-projeto.appspot.com",
+  messagingSenderId: "0000000000",
+  appId: "1:0000000000:web:abc123"
+};
